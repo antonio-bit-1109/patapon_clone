e05377667c635e58c16ffb45c039620261b4d138
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-09-09 22:50:23 +0200

    Integrate action handling in `Gameplay` using `ActionsManager` and add support for movement, attack, defend, and jump actions.

--- src/game/scenes/Gameplay.ts
+++ src/game/scenes/Gameplay.ts
@@ -1,9 +1,10 @@
-import {sceneName} from "../global/global_constant.ts";
+import {actions, sceneName} from "../global/global_constant.ts";
 import {IData} from "../global/interface.ts";
 import {EnvironmentManager} from "../manager/EnvironmentManager.ts";
 import {DudesArmyManager} from "../manager/DudesArmyManager.ts";
 import {InputKeyboardManager} from "../manager/InputKeyboardManager.ts";
 import {SoundsManager} from "../manager/SoundsManager.ts";
+import {ActionsManager} from "../manager/ActionsManager.ts";
 
 
 export class Gameplay extends Phaser.Scene {
@@ -12,13 +13,14 @@ export class Gameplay extends Phaser.Scene {
     private environmentManager: EnvironmentManager;
     private dudesArmyManager: DudesArmyManager;
     private inputKeyboardManager: InputKeyboardManager;
+    private actionsManager: ActionsManager;
 
     constructor() {
         super(sceneName.gameplay);
         this.environmentManager = new EnvironmentManager(this);
         this.dudesArmyManager = new DudesArmyManager(this);
         this.inputKeyboardManager = new InputKeyboardManager(this);
-
+        this.actionsManager = new ActionsManager(this);
     }
 
     init(data: IData) {
@@ -27,6 +29,7 @@ export class Gameplay extends Phaser.Scene {
 
     preload() {
     }
+
 
     create() {
 
@@ -38,8 +41,40 @@ export class Gameplay extends Phaser.Scene {
     }
 
 
-    update() {
-        // this.environmentManager.update()
+    update(time: number, delta: number) {
+
+        this.actionsManager.update(this.inputKeyboardManager.getInputsContainer())
+
+        if (this.actionsManager.getActionToPerform()) {
+
+            let action = this.actionsManager.getActionToPerform()
+
+            switch (action) {
+                case actions.move :
+                    this.dudesArmyManager.moveDudes(this.actionsManager)
+                    this.environmentManager.moveTerrain()
+                    this.environmentManager.moveBackground()
+                    break;
+
+                case actions.attack :
+                    this.dudesArmyManager.attackDudes()
+                    break;
+
+                case actions.defend :
+                    this.dudesArmyManager.defendDudes()
+                    break;
+
+                case actions.jump :
+                    this.dudesArmyManager.jumpDudes()
+                    break;
+                default:
+                    this.dudesArmyManager.idleDudes(this.actionsManager)
+                    console.log("nessuna azione dudes in idle")
+                    break;
+            }
+            this.actionsManager.resetActionToPerform();
+            this.inputKeyboardManager.resetInputsContainer()
+        }
 
     }
 }
