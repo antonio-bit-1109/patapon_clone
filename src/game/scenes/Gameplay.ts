b50c996c4fdd0950d8e9610b11238f9744d20131
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-09-09 17:39:56 +0200

    Add InputKeyboardManager to Gameplay scene for managing keyboard inputs

--- src/game/scenes/Gameplay.ts
+++ src/game/scenes/Gameplay.ts
@@ -2,20 +2,22 @@ import {sceneName} from "../global/global_constant.ts";
 import {IData} from "../global/interface.ts";
 import {EnvironmentManager} from "../manager/EnvironmentManager.ts";
 import {DudesArmyManager} from "../manager/DudesArmyManager.ts";
+import {InputKeyboardManager} from "../manager/InputKeyboardManager.ts";
 
 
 export class Gameplay extends Phaser.Scene {
 
     private oldDudesTypes: string[] = []
-    // private dudesArmyGameplay_group: Phaser.GameObjects.Group;
     private environmentManager: EnvironmentManager;
     private dudesArmyManager: DudesArmyManager;
-
+    private inputKeyboardManager: InputKeyboardManager;
 
     constructor() {
         super(sceneName.gameplay);
         this.environmentManager = new EnvironmentManager(this);
-        this.dudesArmyManager = new DudesArmyManager(this)
+        this.dudesArmyManager = new DudesArmyManager(this);
+        this.inputKeyboardManager = new InputKeyboardManager(this);
+
     }
 
     init(data: IData) {
@@ -29,11 +31,12 @@ export class Gameplay extends Phaser.Scene {
 
         this.environmentManager.create();
         this.dudesArmyManager.create(this.oldDudesTypes)
-
+        this.inputKeyboardManager.createInputKeys()
     }
 
 
     update() {
         this.environmentManager.update()
+     
     }
 }
