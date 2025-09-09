464e3bdfe667c17830594bd0f218f18958d00450
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-09-09 12:52:37 +0200

    Fix incorrect import path for SoundsManager in ChooseMainCharacter scene

--- src/game/scenes/ChooseMainCharacter.ts
+++ src/game/scenes/ChooseMainCharacter.ts
@@ -1,7 +1,7 @@
 import {sceneName} from "../global/global_constant.ts";
 import {CommonMethodsClass} from "./CommonMethodsClass.ts";
 import Sprite = Phaser.GameObjects.Sprite;
-import {SoundsManager} from "./SoundsManager.ts";
+import {SoundsManager} from "../manager/SoundsManager.ts";
 
 
 export class ChooseMainCharacter extends Phaser.Scene {
