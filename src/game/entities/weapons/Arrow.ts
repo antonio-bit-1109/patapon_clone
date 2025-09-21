b1763e10758f037b84d127286c68cf44aa60c910
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-09-21 16:34:08 +0200

    Add `range` property to `Arrow` class with getter and setter methods to manage weapon range.

--- src/game/entities/weapons/Arrow.ts
+++ src/game/entities/weapons/Arrow.ts
@@ -3,13 +3,21 @@ import {GeneralWeapon} from "./GeneralWeapon.ts";
 export class Arrow extends GeneralWeapon {
 
     private damage: number = 4;
-
+    private range: number = 1;
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
         super(scene, x, y, texture);
         scene.physics.add.existing(this);
         scene.add.existing(this);
 
+    }
+
+    public getRange() {
+        return this.range
+    }
+
+    public setRange(val: number) {
+        this.range = val;
     }
 
     public getDamage() {
